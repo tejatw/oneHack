81ed48ac450fb8ec3cb42591221944babbba0f42
tejatw <tejatw@gmail.com>
2016-09-22 23:46:54 +0530

    Update tescoLite.js

--- tescoLite.js
+++ tescoLite.js
@@ -6,25 +6,23 @@ var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static('public'));
-
-//app.get('/index.htm', function (req, res) {
-//   res.sendFile( __dirname + "/" + "index.htm" );
-//})
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.post('/tescoLite', urlencodedParser, function (req, res) {
 
    var conversation = watson.conversation({
-     username: '34021464-26ac-4021-bcf7-cc957d666027',
-     password: 'eUqt0sbKnAfi',
+     username: 'c9941228-7439-40a0-b5b4-257e8d12ec41',
+     password: 'LRcQoVvpGmii',
      version: 'v1',
      version_date: '2016-07-11'
    });
 
         var context = {};
+console.log(req.body.textFromUser);
 
         conversation.message({
-          workspace_id: 'bdd0345f-712a-4171-8f1b-33e8782f232d',
+          workspace_id: '6f038651-7a41-44c3-88fc-6f73547c6723',
           input: {'text': req.body.textFromUser},
           context: context
         },  function(err, response) {
@@ -34,16 +32,11 @@ app.post('/tescoLite', urlencodedParser, function (req, res) {
             console.log(JSON.stringify(response, null, 2));
             res.end(JSON.stringify(response));
         });
-   // Prepare output in JSON format
-   //res.end(JSON.stringify(""));
 })
 
 var server = app.listen(8081, function () {
 
   var host = server.address().address
   var port = server.address().port
-
   console.log("Example app listening at http://%s:%s", host, port)
-
 })
-
